b1b9e2574104816916359d0cf3fc77b66c31261c
k-olejnicki <kamil.olejnicki@gmail.com>
2016-04-18 00:38:50 +0200

    reg disable

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,11 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable
+  if Rails.env.production?
+    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  else
+    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable
+  end
   has_many :images
   has_many :comments
 end
