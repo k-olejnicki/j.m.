cbf6003ec932f5ee3b5ca245c5a6211fbbac4174
k-olejnicki <kamil.olejnicki@gmail.com>
2016-06-12 16:48:22 +0200

    clean dir

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,11 +1,11 @@
 class MessageMailer < ApplicationMailer
 
-  default from: "Your Mailer <noreply@yourdomain.com>"
+  default from: "Formularz Kontaktowy Strony"
   default to: "Justyna Miazga <justynamiazga1992@gmail.com>"
 
   def new_message(message)
     @message = message
-    mail subject: "Message from #{message.name} #{message.tel} #{message.email}"
+    mail subject: "Message from #{message.name} [#{message.tel}] <#{message.email}>"
   end
 
 end
