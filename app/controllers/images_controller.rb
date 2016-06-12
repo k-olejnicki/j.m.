5192ac9728d36d3eb0cc6ff7b910b2a4c127408e
k-olejnicki <kamil.olejnicki@gmail.com>
2016-06-12 12:07:42 +0200

    fixing foto

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -39,7 +39,7 @@ class ImagesController < ApplicationController
 
     respond_to do |format|
       if @image.save
-        format.html { redirect_to @image, notice: 'Image was successfully created.' }
+        format.html { redirect_to '/admin', notice: 'Zdjęcie dodano pomyślnie.' }
         format.json { render :show, status: :created, location: @image }
       else
         format.html { render :new }
@@ -53,7 +53,7 @@ class ImagesController < ApplicationController
   def update
     respond_to do |format|
       if @image.update(image_params)
-        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
+        format.html { redirect_to '/admin', notice: 'Zdjęcie zaktualizowano pomyślnie.' }
         format.json { render :show, status: :ok, location: @image }
       else
         format.html { render :edit }
@@ -67,7 +67,7 @@ class ImagesController < ApplicationController
   def destroy
     @image.destroy
     respond_to do |format|
-      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
+      format.html { redirect_to '/admin', notice: 'Zdjęcię pomyślnie usunięto.' }
       format.json { head :no_content }
     end
   end
