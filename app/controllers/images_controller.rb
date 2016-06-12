e468c3b5c23830bbb2117032f198d459cbd97b8f
k-olejnicki <kamil.olejnicki@gmail.com>
2016-06-12 12:40:17 +0200

    fixing aws

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,5 @@
 class ImagesController < ApplicationController
+  before_action :set_image, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_admin!, except: [:index, :show]
 
   # GET /images
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
       :storage => :s3,
+      :s3_region=> 'eu-central-1',
       :s3_credentials => {
           :bucket => ENV['AWS_BUCKET'],
           :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
